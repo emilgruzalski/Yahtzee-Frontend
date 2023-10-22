bb97720074944310dd35d2ddb6a7cc8d1865862c
matbmp <mbiernacki531@gmail.com>
2023-10-22 21:28:00 +0200

    Modify profile fix.

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -10,25 +10,52 @@
 		coins: 100000000
 	};
 
+	let isEditingUsername: boolean = false;
 	let isEditingEmail: boolean = false;
+
+	function startEditUsername() {
+		isEditingUsername = true;
+	}
+
+	function endEditUsername() {
+		isEditingUsername = false;
+	}
+
+	function startEditEmail() {
+		isEditingEmail = true;
+	}
+
+	function endEditEmail() {
+		isEditingEmail = false;
+	}
 </script>
 
 <div class="h-full flex items-center justify-center">
 	<div
 		class="flex flex-col items-center p-4 mx-4 w-full xl:w-1/2 h-5/6 rounded-lg border border-gray-400 bg-gray-100 shadow-lg"
 	>
-		<div class="text-7xl p-4">{user.username}</div>
+		<div class="text-2xl text-gray-700">
+			{#if isEditingUsername}
+				<input class="field" bind:value={user.username} />
+				<button class="btn-primary" on:click={endEditUsername}>Save</button>
+			{:else}
+				<span class="">{user.username}</span>
+				<button class="btn-primary" on:click={startEditUsername}>Change</button>
+			{/if}
+		</div>
+		
+
 		<div class="text-2xl text-gray-700">
 			{#if isEditingEmail}
-				<input class='field' bind:value={user.email}/>
-				<button class="btn-primary">Save</button>
+				<input class="field" bind:value={user.email} />
+				<button class="btn-primary" on:click={endEditEmail}>Save</button>
 			{:else}
 				{user.email}
-				<button class="btn-primary" on:click={() => isEditingEmail = true}>Change</button>
+				<button class="btn-primary" on:click={startEditEmail}>Change</button>
 			{/if}
 		</div>
 
-		<a href="/user/modify" class="btn-primary mt-6">Modify profile</a>
+		<a href="/user/modify" class="btn-primary mt-6">Change password</a>
 
 		<div class="flex-grow flex flex-col items-center justify-center">
 			<div class="min-w-0">
--- src/routes/user/modify/+page.svelte
+++ src/routes/user/modify/+page.svelte
@@ -1,29 +1,19 @@
 <script lang="ts">
-    interface UserData {
-        username: string,
-        email: string
-        coins: number
-    }
-    let user: UserData = {
-        username: "matbmp",
-        email: "matbmp@gmail.com",
-        coins: 100000000
-    }
-    let password: string
+
+    let password: string = ""
+    let confirmPassword: string = ""
 </script>
 
 <div class="h-full flex items-center justify-center">
     <div class='flex flex-col rounded-md border border-gray-400 bg-gray-100 shadow-lg p-8 xl:w-1/4 lg:w-1/3 w-full m-4'>
-        
-        <label for="email" class='py-2'>Email</label> 
-        <input type="email" bind:value={user.email} class='field'/>
-
-        <label for="username" class='py-2'>Username</label> 
-        <input type="text" bind:value={user.username} class='field'/>
 
         <label for="password" class='py-2'>Password</label> 
         <input type="password" bind:value={password} class='field'/>
 
+        <label for="password" class='py-2'>Confirm password</label> 
+        <input type="password" bind:value={confirmPassword} class='field'/>
+
         <a href="/register" class='btn-primary-full mt-6'>Save changes</a>
+        <a href="/register" class='btn-primary mt-6'>Back</a>
     </div>
 </div>
